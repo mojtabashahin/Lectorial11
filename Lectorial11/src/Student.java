68579892b7d3c82d8edb0f0c99371163177f5879
E116324 <E116324@localhost>
2022-10-05 12:05:28 +1100

    Add the Sudent class

---
+++ Lectorial11/src/Student.java
@@ -0,0 +1,16 @@
+
+public class Student {
+	
+	private String name;
+	private int ID;
+	public Student (String nStudent, int idStudent)
+	{
+		name=nStudent;
+		ID= idStudent;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
